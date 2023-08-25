a4015ce4fe836366e6117710079e26443d7f8d42
devhindo <ahmedhindawi2001@gmail.com>
2023-08-25 21:03:15 +0300

    configured ngrok

--- src/main.go
+++ src/main.go
@@ -30,47 +30,45 @@ func main() {
 		panic(err)
 	}
 
-	client_id := os.Getenv("CLIENT_ID")
 
-	auth_url := ""
-	auth_scopes := "tweet.read%20tweet.write%20users.read%20users.read%20follows.read%20follows.write%20offline.access"
-	redirect_url := "https://github.com/devhindo/x"
-
-	auth_url += "https://twitter.com/i/oauth2/authorize?response_type=code&client_id="
-	auth_url += client_id
-	auth_url += "&redirect_uri=" + redirect_url
-
-	auth_url += "&scope=" + auth_scopes
-
-	code_challenge := generateRandomString(12)
-	state := generateRandomString(12)
-	auth_url += "&state=" + state + "&code_challenge=" + code_challenge + "&code_challenge_method=plain"
-
-	fmt.Println(auth_url)
+	
 
 	//os.Getenv("TWITTER_API_KEY")
 	//os.Getenv("TWITTER_API_SECRET_KEY")
-
-	if err := run(context.Background()); err != nil {
+	ngrok_error, tun_URL := run(context.Background())
+	if ngrok_error != nil {
 		log.Fatal(err)
 	}
+	log.Println(tun_URL)
+	log.Println(tun_URL)
+	log.Println(tun_URL)
+	log.Println(tun_URL)
+	log.Println(tun_URL)
+	// get the ngrok url
 
+	
+
+
+	
 
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context) (error, string) {
 	authToken := os.Getenv("NGROK_AUTHTOKEN")
 	tun, err := ngrok.Listen(ctx,
 		config.HTTPEndpoint(),
 		ngrok.WithAuthtoken(authToken),
+		// todo set ngrok-skip-browser-warning to anyvalue
 	)
 	if err != nil {
-		return err
+		return err, ""
 	}
-
+	
 	log.Println("tunnel created:", tun.URL())
 
-	return http.Serve(tun, http.HandlerFunc(handler))
+	construct_auth_url(tun.URL())
+
+	return http.Serve(tun, http.HandlerFunc(handler)), tun.URL()
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -80,7 +78,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 
 
+func construct_auth_url(redirect_url string) {
+	auth_url := ""
+	auth_scopes := "tweet.read%20tweet.write%20users.read%20users.read%20follows.read%20follows.write%20offline.access"
+	client_id := os.Getenv("CLIENT_ID")
+	auth_url += "https://twitter.com/i/oauth2/authorize?response_type=code&client_id="
+	auth_url += client_id
+	auth_url += "&redirect_uri=" + redirect_url
+	auth_url += "&scope=" + auth_scopes
+	code_challenge := generateRandomString(12)
+	state := generateRandomString(12)
+	auth_url += "&state=" + state + "&code_challenge=" + code_challenge + "&code_challenge_method=plain"
 
+	fmt.Println(auth_url)
+}
 
 
 
