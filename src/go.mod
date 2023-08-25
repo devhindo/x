725baa5123f947ac17341f44367d04ae06c10a35
devhindo <ahmedhindawi2001@gmail.com>
2023-08-25 19:37:37 +0300

    added ngrok client

--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,22 @@ module github.com/devhindo/x
 go 1.21.0
 
 require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/michimani/gotwi v0.14.0 // indirect
+	github.com/joho/godotenv v1.5.1
+	golang.ngrok.com/ngrok v1.3.1
+	golang.org/x/crypto v0.9.0
+)
+
+require (
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/inconshreveable/log15 v3.0.0-testing.3+incompatible // indirect
+	github.com/inconshreveable/log15/v3 v3.0.0-testing.5 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.ngrok.com/muxado/v2 v2.0.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/term v0.8.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	//"context"
+	"context"
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
+	"log"
+	"net/http"
 
 	//"github.com/michimani/gotwi"
 	//"github.com/michimani/gotwi/fields"
@@ -15,6 +18,8 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"golang.ngrok.com/ngrok"
+	"golang.ngrok.com/ngrok/config"
 )
 
 func main() {
@@ -25,18 +30,18 @@ func main() {
 		panic(err)
 	}
 
-	client_id := os.Getenv("CLIENT_ID")	
+	client_id := os.Getenv("CLIENT_ID")
 
 	auth_url := ""
 	auth_scopes := "tweet.read%20tweet.write%20users.read%20users.read%20follows.read%20follows.write%20offline.access"
 	redirect_url := "https://github.com/devhindo/x"
-	
+
 	auth_url += "https://twitter.com/i/oauth2/authorize?response_type=code&client_id="
 	auth_url += client_id
 	auth_url += "&redirect_uri=" + redirect_url
-	
+
 	auth_url += "&scope=" + auth_scopes
-	
+
 	code_challenge := generateRandomString(12)
 	state := generateRandomString(12)
 	auth_url += "&state=" + state + "&code_challenge=" + code_challenge + "&code_challenge_method=plain"
@@ -46,16 +51,47 @@ func main() {
 	//os.Getenv("TWITTER_API_KEY")
 	//os.Getenv("TWITTER_API_SECRET_KEY")
 
-	
+	if err := run(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+
+
 }
+
+func run(ctx context.Context) error {
+	authToken := os.Getenv("NGROK_AUTHTOKEN")
+	tun, err := ngrok.Listen(ctx,
+		config.HTTPEndpoint(),
+		ngrok.WithAuthtoken(authToken),
+	)
+	if err != nil {
+		return err
+	}
+
+	log.Println("tunnel created:", tun.URL())
+
+	return http.Serve(tun, http.HandlerFunc(handler))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from ngrok-go!")
+}
+
+
+
+
+
+
+
+
+
 
 func generateRandomString(length int) string {
 	b := make([]byte, length)
 	_, err := rand.Read(b)
 	if err != nil {
-	   panic(err)
+		panic(err)
 	}
 	// return only characters in base64 alphabet, removes characters that are not url safe
 	return base64.RawURLEncoding.EncodeToString(b)
- }
- 
+}
