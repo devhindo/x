39228b29546b9be6e0ecf5da60ba7f9a7ddbbdd1
hindawi <ahmedhindawi2001@gmail.com>
2023-09-17 07:14:46 +0300

    added verification

--- src/x/src/app/verification/page.tsx
+++ src/x/src/app/verification/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      x cli
+      verification
     </main>
   )
 }
