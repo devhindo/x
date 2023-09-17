05eac7cab08d7ce9f9c47480d37919a79c44d4ff
devhindo <ahmedhindawi2001@gmail.com>
2023-09-17 09:51:10 +0300

    deleted expose server

--- src/server/main.go
+++ src/server/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, world!")
+        fmt.Fprintf(w, "Hello, x server 👋!")
     })
 
     http.ListenAndServe(":3000", nil)
