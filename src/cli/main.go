6d874f6e270e64ba27c9ff4bfffde1fb9d3864ca
devhindo <ahmedhindawi2001@gmail.com>
2023-09-19 07:44:56 +0300

    idk

--- src/cli/main.go
+++ src/cli/main.go
@@ -1,11 +1,50 @@
 package main
 
 import (
-	"github.com/devhindo/x/src/cli/x"
+	//"github.com/devhindo/x/src/cli/x"
+	"net/http"
+	"fmt"
+	"io/ioutil"
 )
 
+type User struct {
+	Username string
+	Secret string
+}
+
 func main() {
-	x.Run()
+	//x.Run()
+	url := "http://localhost:3000/api/learn"
+
+	// Create a new HTTP request object.
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    // Send the request and receive the response.
+    resp, err := http.DefaultClient.Do(req)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    // Read the response body.
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    // Close the response body.
+    defer resp.Body.Close()
+
+    // Print the response body.
+    fmt.Println(string(body))
+
+
+
 }
 
 
--- src/server/src/app/api/learn/route.ts
+++ src/server/src/app/api/learn/route.ts
@@ -1,3 +1,10 @@
 export async function GET(request: Request) {
+    // wait 10 seconds
+    // console log comming requests
+    //console.log(request);
+    console.log("___________________________________");
+    console.log(request.body);
+    console.log("___________________________________");
+    console.log(request.headers);
     return new Response("learn page");
 }
