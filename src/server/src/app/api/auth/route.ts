6d994be65276868982c1cf44e09ca8ddaa9d2765
devhindo <ahmedhindawi2001@gmail.com>
2023-09-21 14:22:03 +0300

    req access token

--- src/server/src/app/api/auth/route.ts
+++ src/server/src/app/api/auth/route.ts
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js'
 export async function GET(request: Request) {
     console.log("start of requesting access token")
     const { searchParams } = new URL(request.url)
-    
+
     const state = searchParams.get("state")
     const code = searchParams.get("code")
 
@@ -21,12 +21,12 @@ export async function GET(request: Request) {
 
     const user = await get_user_data(state)
 
-    const { code_verifier } = user
+    const { code_verifier, code_challenge } = user
 
     // request access token from twitter api
-    //const access_token = await get_access_token(code,code_verifier)
-    const access_token = await req_access_token(code,code_verifier)
-    console.log("access_token:" + access_token)
+    //const access_token = await ax(code,code_verifier)
+    const access_token = await req_access_token(code, code_verifier)
+    //console.log(code_verifier)
 
     // todo: save all this in supabase
 
@@ -37,44 +37,31 @@ export async function GET(request: Request) {
 }
 
 
-
-//type User = {
-//    username?: string
-//    secret?:string
-//}
-//
-//export async function POST(request: Request) {
-//    const data: User = await request.json()
-//    console.log(data)
-//
-//    const { username, secret } = data
-//
-//    return NextResponse.json({ username, secret })
-//}
-
-
-// Create a single supabase client for interacting with your database
-
-
-const supabase = createClient(process.env.SUPABASE_URL as string , process.env.SUPABASE_SECRET as string)
-
 type User = {
     state?: string
     code_verifier?: string
     code_challenge?: string
 }
 
+
+// Create a single supabase client for interacting with your database
+
+
+const supabase = createClient(process.env.SUPABASE_URL as string, process.env.SUPABASE_SECRET as string)
+
+
+
 async function check_if_state_valis(state: string) {
     const { data, error } = await supabase
-    .from('users')
-    .select()
-    .eq('state', state)
-    .maybeSingle()
+        .from('users')
+        .select()
+        .eq('state', state)
+        .maybeSingle()
 
     if (error) {
         console.log(error)
     }
-    if(data) {
+    if (data) {
         return true
     }
     return false
@@ -82,71 +69,19 @@ async function check_if_state_valis(state: string) {
 
 async function get_user_data(state: string) {
     const { data, error } = await supabase
-    .from('users')
-    .select()
-    .eq('state', state)
-    .maybeSingle()
+        .from('users')
+        .select()
+        .eq('state', state)
+        .maybeSingle()
 
     if (error) {
         console.log(error)
     }
-    if(data) {
+    if (data) {
         return data
     }
     return null
 }
-
-import axios from 'axios'
-
-async function get_access_token(code: string, code_verfier: string) {
-    const url = 'https://api.twitter.com/oauth2/token'
-    const data = new URLSearchParams()
-    data.append('code', code)
-    data.append('grant_type', 'authorization_code')
-    data.append('client_id', process.env.TWITTER_CLIENT_ID as string)
-    data.append('redirect_uri', process.env.TWITTER_REDIRECT_URI as string)
-    data.append('code_verifier', code_verfier)
-
-    axios.post(url, data, {
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    })
-    .then(response => {
-        console.log(response.data)
-    }
-    )
-    .catch(error => {
-        console.log(error)
-    }
-    )
-}
-
-// insert user
-/*
-interface User {
-    username: string,
-    access_token: string
-}
-
-let user = {} as User
-user.username = 'test'
-user.access_token = 'test'
-
-const insertUser = async (user: any) => {
-    const { data, error } = await supabase.from('users').insert([
-        { username: user.username, access_token: user.access_token},
-    ])
-    if (error) {
-        console.log(error)
-    }
-    console.log(data)
-}
-*/
-// insertUser(user)
-
-
-
 
 
 /*
@@ -158,52 +93,81 @@ curl --location --request POST 'https://api.twitter.com/2/oauth2/token' \
 --data-urlencode 'redirect_uri=https://www.example.com' \
 --data-urlencode 'code_verifier=challenge'
 */
-/*
-async function get_access_token(code: string) {
-    const data = {
-        code: code,
-        grant_type: 'authorization_code',
-        client_id: process.env.TWITTER_CLIENT_ID,
-        redirect_uri: process.env.TWITTER_REDIRECT_URI,
-        code_verifier: process.env.TWITTER_CODE_VERIFIER
-    }
 
-    const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: JSON.stringify(data)
-    }
+// todo: https://twittercommunity.com/t/trying-to-get-oauth-2-0-token-receiving-missing-valid-authorization-header-error/163633/3 || 'Authorization: Basic CONFIDENTIAL_CLIENT_AUTH_HEADER'
+// either here or here
 
-    fetch('https://api.twitter.com/2/oauth2/token', options)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
-
-
-    return NextResponse.json({ data })
+function generate_CONFIDENTIAL_CLIENT_AUTH_HEADER() {
+    const CLIENT_ID = process.env.CLIENT_ID
+    const CLIENT_SECRET = process.env.CLIENT_SECRET
+    return Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString('base64')
 }
-*/ 
 
-async function req_access_token(code: string, code_verfier: string) {
-    const url = 'https://api.twitter.com/2/oauth2/token'
-    const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
-    const data = {
-        grant_type: 'authorization_code',
-        code: code,
-        client_id: process.env.TWITTER_CLIENT_ID,
-        redirect_uri: process.env.TWITTER_REDIRECT_URI,
-        code_verifier: code_verfier
-    }
+import axios from "axios"
 
-    const options = {
+async function ax(code: string, verfier: string) {
+    var options = {
         method: 'POST',
-        headers: headers,
-        body: JSON.stringify(data)
-    }
+        url: 'https://api.twitter.com/2/oauth2/token',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + generate_CONFIDENTIAL_CLIENT_AUTH_HEADER(),
+        },
+        data: new URLSearchParams({
+            grant_type: 'authorization_code',
+            client_id: process.env.CLIENT_ID,
+            code_verifier: verfier,
+            code: code,
+            redirect_uri: 'http://localhost:3000/api/auth',
+        })
+    };
 
-    const response = await fetch(url, options)
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    }
+    );
+}
+/*
+POST https://authorization-server.com/token
+
+grant_type=authorization_code
+&client_id=i1zzqeXYK_4ZeAEU5_u6b8Qf
+&client_secret=UaOzV9MrAijJMDmhucCBRo2XrRlPIfqBg1b0HCXePsDyqm88
+&redirect_uri=https://www.oauth.com/playground/authorization-code-with-pkce.html
+&code=HV24toek3g0qCYg3zZZcC4lSjE4IlI3nAZBRAK3thKyGeVQT
+&code_verifier=mdUIOYqdl9r5EnV0hAoB6zznhaxqGTY-rXu-jQRwpDL5BE86
+*/
+
+// todo send the code verfier properly
+
+async function req_access_token(code: string, verfier: string) {
+    console.log("xxxxxxxxxxxxxxx" + verfier)
+    let url = 'https://api.twitter.com/2/oauth2/token'
+    url += '?grant_type=authorization_code'
+    url += '&client_id=' + process.env.CLIENT_ID
+    url += '&client_secret=' + process.env.CLIENT_SECRET
+    url += '&redirect_uri=' + 'http://localhost:3000/api/auth'
+    url += '&code=' + code
+    url += '&code_verifier=' + verfier
+
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + generate_CONFIDENTIAL_CLIENT_AUTH_HEADER(),
+        },
+        //body: JSON.stringify({
+        //    grant_type: 'authorization_code',
+        //    client_id: process.env.CLIENT_ID as string,
+        //    client_secret: process.env.CLIENT_SECRET as string,
+        //    redirect_uri: 'http://localhost:3000/api/auth/validate/success',
+        //    code: code,
+        //    code_verifier: verfier,
+        //})
+    })
     const json = await response.json()
     console.log(json)
     return json
