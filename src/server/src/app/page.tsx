bca44ee0ea1430cf8af3cd0c28080c72b77a93cf
devhindo <ahmedhindawi2001@gmail.com>
2023-09-23 16:58:26 +0300

    added some styles

--- src/server/src/app/page.tsx
+++ src/server/src/app/page.tsx
@@ -1,10 +1,32 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      x cli app
-    </main>
+    <div>
+      <main className={styles.main}>
+        X CLI
+        <p>tweet from terminal and stuff</p>
+        <fieldset>
+          <legend>Installation</legend>
+          <ul>
+            <li></li>
+          </ul>
+        </fieldset>
+        <fieldset>
+          <legend>Authorization</legend>
+          <ul>
+            <li>Run <span className={styles.cmd}>x auth</span></li>
+            <li>Then <span className={styles.cmd}>x auth -v</span> or <span className={styles.cmd}>x auth --validate</span></li>
+          </ul>
+        </fieldset>
+        <fieldset>
+          <legend>commands</legend>
+          <ul>
+            <li>tweet: <span className={styles.cmd}>x -t "hello, xcli!"</span> or <span className={styles.cmd}>x --tweet "hello, xcli!"</span></li>
+          </ul>
+        </fieldset>
+      </main>
+      <h1><a href="https://github.com/devhindo/x" target='_blank'>source code</a></h1>
+    </div>
   )
 }
