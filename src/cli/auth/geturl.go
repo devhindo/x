bcd9a88017556d2436d4774e0f1468f19663ef02
devhindo <ahmedhindawi2001@gmail.com>
2023-09-24 14:52:43 +0300

    fixed go errors

--- src/cli/auth/geturl.go
+++ src/cli/auth/geturl.go
@@ -1,12 +1,8 @@
 package auth
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
-	"io"
 
 	"github.com/devhindo/x/src/cli/lock"
 )
@@ -22,41 +18,4 @@ func Get_url_db() {
 	k := License{License: l}
 
 	postL(url, k)
-}
-
-type License struct {
-	License string `json:"license"`
-}
-
-type response struct {
-    Message string `json:"message"`
-}
-
-func postL(url string, l License) {
-
-    jsonBytes, err := json.Marshal(l)
-    if err != nil {
-        panic(err)
-    }
-
-    resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonBytes))
-
-    if err != nil {
-        panic(err)
-    }
-
-    defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			//Failed to read response.
-			panic(err)
-		}
-
-		var r response
-
-		err = json.Unmarshal(body, &r)
-
-		//Convert bytes to String and print
-		fmt.Println(r.Message)
 }
--- src/cli/auth/url.go
+++ src/cli/auth/url.go
@@ -1,16 +1,5 @@
 package auth
 
-import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-
-	"github.com/devhindo/x/src/cli/clear"
-	"github.com/devhindo/x/src/cli/lock"
-)
-
 func (u *User) generate_auth_url() {
 	auth_url := ""
 	auth_scopes := "tweet.read%20tweet.write%20users.read%20users.read%20follows.read%20follows.write%20offline.access"
@@ -21,58 +10,4 @@ func (u *User) generate_auth_url() {
 	auth_url += "&scope=" + auth_scopes
 	auth_url += "&state=" + u.State + "&code_challenge=" + u.Code_challenge + "&code_challenge_method=S256"
 	u.Auth_URL = auth_url
-}
-
-func Get_url_db() {
-	get_url_from_db()
-}
-
-func get_url_from_db() {
-
-	lic, err := lock.ReadLicenseKeyFromFile()
-	if err != nil {
-		fmt.Println("couldn't generate url | try 'x auth'")
-		return
-	}
-
-	l := clear.License{
-		License: lic,
-	}
-
-	url := "https://x-blush.vercel.app/api/user/url"
-
-	req, err := http.NewRequest("POST", url, nil)
-
-	if err != nil {
-		panic(err)
-	}
-
-	jsonBytes, err := json.Marshal(l)
-	if err != nil {
-		panic(err)
-	}
-
-	req.Body = io.NopCloser(bytes.NewBuffer(jsonBytes))
-
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	defer resp.Body.Close()
-
-	var response struct {
-		AuthURL string `json:"auth_url"`
-	}
-
-	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
-		panic(err)
-	}
-
-	fmt.Println(response.AuthURL)
-
 }
