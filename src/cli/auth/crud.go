621bedd7caff8db6494c8c622a7d1e4e85942ed7
devhindo <ahmedhindawi2001@gmail.com>
2023-09-24 12:48:15 +0300

    fixed dumb vercel error

--- src/cli/auth/crud.go
+++ src/cli/auth/crud.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-    "log"
     "encoding/json"
 
 	"github.com/devhindo/x/src/cli/lock"
@@ -13,45 +12,37 @@ import (
 )
 
 func POST(url string, u User) {
-	// Create a new HTTP request object.
-	req, err := http.NewRequest("POST", url, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
+		
 	jsonBytes, err := json.Marshal(u)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-
-	req.Body = io.NopCloser(bytes.NewBuffer(jsonBytes))
-
-	req.Header.Set("Content-Type", "application/json")
-
-	// Send the request
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonBytes))
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+
 	defer resp.Body.Close()
 
-	// Handle the response
+	status := resp.StatusCode
+	fmt.Println(status)
+
 	//body, err := io.ReadAll(resp.Body)
 	//if err != nil {
-	//	log.Fatal(err)
+    //		panic(err)
 	//}
 
+	//Convert bytes to String and print
+	//jsonStr := string(body)
+	//fmt.Println("Response: ", jsonStr)
 
-	
-	status := resp.StatusCode
 	
 	if status != 200 {
 		fmt.Println("error adding user")
 	} else {
+		
 		err := lock.WriteLicenseKeyToFile(u.License)
 		if err != nil {
 			fmt.Println("coudln't write license key to file")
@@ -59,7 +50,6 @@ func POST(url string, u User) {
 		}
 	}
 
-    //fmt.Println(string(body))
 }
 
 func GET(url string) {
