fbb24b37b6a35d74fd89476aa4897c30b2186948
devhindo <ahmedhindawi2001@gmail.com>
2023-09-24 14:12:41 +0300

    indented tweet api

--- src/server/src/app/api/tweets/post/route.ts
+++ src/server/src/app/api/tweets/post/route.ts
@@ -1,5 +1,4 @@
 import { Client } from "twitter-api-sdk";
-import { AuthClient } from "twitter-api-sdk/dist/types";
 import { NextResponse } from "next/server";
 import { createClient } from '@supabase/supabase-js'
 import { generate_CONFIDENTIAL_CLIENT_AUTH_HEADER } from "../../auth/route";
@@ -35,8 +34,6 @@ export async function POST(request: Request) {
     }
 
     const access_token = await get_access_token(license as string)
-
-    console.log("uppppppppppppperaccess token: " + access_token)
     
     const client = new Client(access_token as string);
     
@@ -49,22 +46,16 @@ export async function POST(request: Request) {
     return NextResponse.json({ message: 'tweet posted!' }, { status: 200 })
 }
 
-        
-
 async function get_access_token(license: string) {
 
     const [refresh_token_exist, refresh_token] = await get_refresh_token(license)
 
     if (!refresh_token_exist) {
-        console.log("refresh token not found")
         return
     }
 
     const [new_access_token, new_refresh_token] = await POST_new_access_token(license, refresh_token)
     
-    console.log("55new access token: " + new_access_token)
-    console.log("55new refresh token: " + new_refresh_token)
-
     await save_new_access_token(license, new_access_token, new_refresh_token)
 
     return new_access_token
@@ -77,11 +68,8 @@ async function save_new_access_token(license: string, access_token: string, refr
         .eq('license', license)
     
     if (error) {
-        console.log("couldn't update new access token" + error)
         return
     }      
-    console.log("new access token saved")
-    //await update_refresh_token(license, refresh_token)
 }
 
 async function update_refresh_token(license: string, refresh_token: string) {
@@ -91,25 +79,9 @@ async function update_refresh_token(license: string, refresh_token: string) {
         .eq('license', license)
 
     if (error) {
-        console.log("couldn't update new refresh token" + error)
         return
     }
-    console.log("new refresh token saved")
 }
-    // I have refresh token
-
-    //const { data, error } = await supabase
-    //    .from('users')
-    //    .select()
-    //    .eq('license', license)
-    //    .single()
-    //if (error) {
-    //    return NextResponse.json({ error, message: 'err' }, { status: 401 })
-    //}
-    //const { access_token } = data
-//
-    //return access_token
-
 
 async function get_refresh_token(license: string): Promise<[boolean,string]> {
     const {data, error} = await supabase
@@ -143,14 +115,8 @@ async function POST_new_access_token(license: string, refresh_old_token: string)
 
     const json = await response.json()
 
-    console.log(json)
-
-
-
     const { access_token, refresh_token } = json
 
-    console.log("newwwwwwwwtoken: " + access_token)
-    console.log("newwwwwwwwrefresh: " + refresh_token)
     if(!access_token || !refresh_token) {
         console.log("new access token not found")
     }
