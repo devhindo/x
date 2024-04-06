6cd3d2941cf91fab7c6739d6ceed1c35328deb71
devhindo <ahmedhindawi2001@gmail.com>
2024-04-07 00:39:50 +0200

    init cobra

---
+++ src/cli/cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "x",
+	Short: "x is a CLI tool for posting tweets",
+	Long: `x is a CLI tool for posting tweets.
+	You can post tweets now or in the future.
+	You can also clear your credentials and start over.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}	
+}
--- src/cli/go.mod
+++ src/cli/go.mod
@@ -3,3 +3,9 @@ module github.com/devhindo/x/src/cli
 go 1.22.2
 
 require github.com/google/uuid v1.3.1
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/cobra v1.8.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
